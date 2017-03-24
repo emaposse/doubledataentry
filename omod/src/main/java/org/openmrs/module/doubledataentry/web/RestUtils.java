6b02797c31e89e19ff636dcdf60d2137763abb3e
Willa Mhawila <a.mhawila@gmail.com>
2017-03-24 16:44:48 -0500

    Created a map of Configuration Class

--- omod/src/main/java/org/openmrs/module/doubledataentry/web/RestUtils.java
+++ omod/src/main/java/org/openmrs/module/doubledataentry/web/RestUtils.java
@@ -1,16 +1,19 @@
 package org.openmrs.module.doubledataentry.web;
 
+import org.apache.commons.collections.map.LinkedMap;
+import org.openmrs.module.doubledataentry.Configuration;
 import org.openmrs.module.htmlformentry.HtmlForm;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by mhawilamhawila on 3/21/17.
  */
 public class RestUtils {
 	
-	public static LinkedHashMap<String, Object> convertHtmlFormToMap(HtmlForm htmlForm) {
-		LinkedHashMap<String, Object> ret = new LinkedHashMap<String, Object>();
+	public static Map<String, Object> convertHtmlFormToMap(HtmlForm htmlForm) {
+		Map<String, Object> ret = new LinkedHashMap<String, Object>();
 		
 		ret.put("formId", htmlForm.getId());
 		if (htmlForm.getForm() != null) {
@@ -26,6 +29,20 @@ public class RestUtils {
 		ret.put("retired", htmlForm.getRetired());
 		ret.put("uuid", htmlForm.getUuid());
 		
+		return ret;
+	}
+
+	public static Map<String,Object> convertConfigurationToMap(Configuration configuration) {
+		Map<String, Object> ret = new LinkedHashMap<String, Object>();
+
+		ret.put("id", configuration.getId());
+		ret.put("htmlForm", convertHtmlFormToMap(configuration.getHtmlForm()));
+		ret.put("frequency", configuration.getFrequency());
+		ret.put("revision", configuration.getRevision());
+		ret.put("published", configuration.getPublished());
+		ret.put("retired", configuration.getRetired());
+		ret.put("uuid", configuration.getUuid());
+
 		return ret;
 	}
 }
--- omod/src/test/java/org/openmrs/module/doubledataentry/web/RestUtilsTest.java
+++ omod/src/test/java/org/openmrs/module/doubledataentry/web/RestUtilsTest.java
@@ -3,11 +3,15 @@ package org.openmrs.module.doubledataentry.web;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Form;
+import org.openmrs.module.doubledataentry.Configuration;
 import org.openmrs.module.doubledataentry.web.RestUtils;
 import org.openmrs.module.htmlformentry.HtmlForm;
 import org.openmrs.util.OpenmrsUtil;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Willa Mhawila on 3/21/17.
@@ -15,20 +19,42 @@ import java.util.LinkedHashMap;
 public class RestUtilsTest {
 	
 	@Test
-	public void convertedHtmlShouldHaveDefinedProperty() {
+	public void convertedHtmlShouldHaveDefinedProperties() {
 		HtmlForm htmlForm = new HtmlForm();
 		String uuid = OpenmrsUtil.generateUid();
 		htmlForm.setUuid(uuid);
 		Form form = new Form();
 		htmlForm.setForm(form);
-		LinkedHashMap<String, Object> converted = RestUtils.convertHtmlFormToMap(htmlForm);
+		Map<String, Object> converted = RestUtils.convertHtmlFormToMap(htmlForm);
 		
-		Assert.assertTrue(converted.containsKey("formId"));
-		Assert.assertTrue(converted.containsKey("uuid"));
-		Assert.assertTrue(converted.containsKey("name"));
-		Assert.assertTrue(converted.containsKey("description"));
-		Assert.assertTrue(converted.containsKey("published"));
-		Assert.assertTrue(converted.containsKey("retired"));
-		Assert.assertTrue(converted.containsKey("version"));
+		assertTrue(converted.containsKey("formId"));
+		assertTrue(converted.containsKey("uuid"));
+		assertTrue(converted.containsKey("name"));
+		assertTrue(converted.containsKey("description"));
+		assertTrue(converted.containsKey("published"));
+		assertTrue(converted.containsKey("retired"));
+		assertTrue(converted.containsKey("version"));
 	}
+
+	@Test
+    public void convertConfigurationToMap_shouldHaveDefinedProperties() {
+        Configuration configuration = new Configuration();
+        HtmlForm htmlForm = new HtmlForm();
+        Form form = new Form();
+        htmlForm.setForm(form);
+        String uuid = OpenmrsUtil.generateUid();
+
+        configuration.setId(1000);
+        configuration.setHtmlForm(htmlForm);
+        configuration.setUuid(uuid);
+
+        Map<String, Object> converted = RestUtils.convertConfigurationToMap(configuration);
+        assertTrue(converted.containsKey("id"));
+        assertTrue(converted.containsKey("uuid"));
+        assertTrue(converted.containsKey("htmlForm"));
+        assertTrue(converted.containsKey("frequency"));
+        assertTrue(converted.containsKey("revision"));
+        assertTrue(converted.containsKey("published"));
+        assertTrue(converted.containsKey("retired"));
+    }
 }
